10e439513f05cb40f093277f40a30f586db38e21
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-01-20 21:50:36 +0530

    Add files via upload

---
+++ arm.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include<conio.h>
+void  main()
+
+{
+ 
+   int t,s=0,r,n;
+  
+  printf("enter no\n");
+
+scanf("%d",&n);
+
+t=n;
+while(n!=0)
+
+{
+
+    r=n%10;
+
+    s=s+(r*r*r);
+
+    n=n/10;
+
+}
+    if(s==t)
+ 
+   {
+
+        printf("armstrong");
+
+    }
+  
+      else
+ 
+   {
+  
+      printf("not armstrong");
+
+    }
+getch();
+
+}
