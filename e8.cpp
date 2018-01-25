d7024a505e302a10740b1215bdea0f700d9291c0
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-01-25 23:04:14 +0530

    Add files via upload

---
+++ e8.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{ 
+   
+ char str;
+ 
+   printf("enter string ");
+  
+  scanf("%c ", &str);
+  
+   if (isalpha(str))
+ 
+   printf("character");
+ 
+   else
+ 
+   printf("not");
+
+getch(); 
+  
+}
