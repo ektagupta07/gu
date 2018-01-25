5096c8d17314f733a8c1006b7ae3b7cbea1e3085
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-01-25 23:10:46 +0530

    Add files via upload

---
+++ e9.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{ 
+   
+ char str;
+ 
+   printf("enter string ");
+  
+  scanf("%c ", &str);
+  
+   if (isdigit(str))
+ 
+   printf("digit");
+ 
+   else
+ 
+   printf("not digit");
+
+getch(); 
+  
+}
