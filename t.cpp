ed0484f9f5601a5c2b67c7f0aba3171674cb8b31
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-01-26 23:55:13 +0530

    Add files via upload

---
+++ t.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+#include<conio.h>
+void main()
+{
+
+  int n,m,h;
+
+  printf("Enter numbers:" );
+
+  scanf("%d",&n);
+
+h=n/60;
+
+m=n%60;
+
+printf("hour=%d,minute=%d",h,m);
+
+ getch();
+
+}
