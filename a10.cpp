985488c36911b362330691d026b37f679bd977a3
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-01-30 22:41:24 +0530

    Add files via upload

---
+++ a10.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[90];
+int i,n;
+printf("enter string ");
+gets(str);
+n=strlen(str);
+for(i=0;i<n;i++)
+{
+if(tolower(str[0]))
+{
+str[0]=toupper(str[0]);
+}
+if(str[i]==' ')
+{
+str[i+1]=toupper(str[i+1]);
+}
+
+}
+printf("%s",str);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
