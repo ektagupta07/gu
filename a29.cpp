0a186990bb7748e4cf91822c2378b348a4eeae43
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-03-18 01:55:11 +0530

    Add files via upload

---
+++ a29.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int n ,a[90],i,j,m,b[90],c=0;
+printf("enter the no");
+scanf("%d%d",&n,&m);
+printf("enter alement");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("enter alement");
+for(i=0;i<m;i++)
+scanf("%d",&b[i]);
+for(i=0;i<n;i++)
+{
+for(j=0;j<m;j++)
+{
+
+if(a[i]==b[j])
+{
+    c++;
+    
+}
+}
+}
+if(c==m)
+{
+printf("yes ");
+}
+else
+{
+printf("no ");
+   
+}
+}
