ca5cf597fe08d21323894071fc2012e526ac6a5c
ektagupta07 <35466405+ektagupta07@users.noreply.github.com>
2018-04-10 18:53:52 +0530

    Add files via upload

---
+++ a35.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,a[90],sum=0;
+printf("enter the no");
+scanf("%d",&n);
+printf("  array element");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+sum=sum+a[i];
+printf("%d",sum);
+}
